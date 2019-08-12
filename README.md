90d268abef15be73b6b6a14b5907c6d5f328d487
unknown <paohe@dev-bj.douyu.tv>
2019-08-12 16:21:23 +0800

    完善路由管理及文档

--- README.md
+++ README.md
@@ -2,8 +2,71 @@
 
 ## 用途
 
-- 为单页面应用开发提供路由状态管理
+- 为单页面应用提供路由状态管理
 
 ## 使用方法
 
 - 在页面内引用
+
+```html
+<script src="./hash-router.js"></script>
+```
+
+- 使用a标签配置路由
+
+```html
+<li>
+    <a href="#first">first page</a>
+    <a href="#second">second page</a>
+    <a href="#third">third page</a>
+</li>
+```
+
+- 使用js调用方法注册路由
+
+```html
+<script>
+    window.onload = function() {
+        // 配置跳转的路由
+        $hashRouter.route('first',()=>{
+            document.getElementById('content').innerHTML='<button>there is the first page</button>';
+        });
+        // 配置404页面
+        $hashRouter.notFound(()=>{
+            document.getElementById('content').innerHTML='<button>404!</button>';
+        });
+        // 初始化
+        $hashRouter.init();
+    }
+</script>
+```
+
+## API
+
+### $hashRouter.init()
+
+· 执行路由初始化工作，在配置好路由后执行。
+
+### $hashRouter.listen(event)
+
+- 对window的事件开启监听，绑定render函数
+
+### $hashRouter.route(path, callback)
+
+- 注册路由，绑定该路由对应的渲染逻辑
+
+### $hashRouter.has(path)
+
+- 判断某个路由是否注册，返回布尔值
+
+### $hashRouter.notFound(callback)
+
+- 配置404页面的渲染逻辑，默认会console.error
+
+### $hashRouter.go(path)
+
+- 主动跳转函数，跳转至某个页面
+
+### $hashRouter.render()
+
+- 执行配置的当前页面的渲染函数
--- hash-router.js
+++ hash-router.js
@@ -4,13 +4,12 @@
     function Router() {
         // 记录跳转历史，是一个栈结构
         this.hashStack = [];
-        // 记录路由信息和对应的callback
-        this.routes = {};
+        // 记录路由信息和对应的callback并初始化404回调
+        this.routes = {
+            '404': () => {console.error('cannot find the page correspond to your current url!');}
+        };
         // 记录当前的url
         this.curUrl = '';
-        // 404
-        this.notFoundPage = '404';
-
     }
 
     // 路由初始化
@@ -22,7 +21,6 @@
 
     // 监听hashchange事件，当路由跳转时会执行render函数
     Router.prototype.listen = function(event) {
-        console.log('listen', event);
         window.addEventListener(event, ()=>this.render());
     }
 
@@ -32,12 +30,13 @@
     }
     // 判断新添加的路由是否已经存在
     Router.prototype.has = function(path) {
-
+        if(this.routes[path]) return true;
+        return false;
     }
 
     // 对外暴露的配置404的方法
-    Router.prototype.notFound = function(nopage) {
-       this.notFoundPage = nopage;
+    Router.prototype.notFound = function(callback) {
+        if(callback) this.routes['404'] = callback;
     }
 
     // 路由跳转方法
@@ -47,14 +46,11 @@
 
     // 路由跳转后执行的渲染方法
     Router.prototype.render = function() {
-        console.log('render', location.hash);
         Router.prototype.notFound();
         this.curUrl = location.hash.slice(1) || '/'; // 'first'
         this.hashStack.push(this.curUrl);
-        if(!this.routes[this.curUrl]) this.notFound();
-        this.routes[this.curUrl]();
+        (!this.routes[this.curUrl]) ? this.routes['404']() : this.routes[this.curUrl]();
     }
-
     // 实例化
     var hashRouter = new Router();
     // 挂载到全局上
